af9e63735b31e90389cdab04d16c8124d5654382
rukshanvishwa <rukshanprakash123@gmail.com>
2025-09-07 10:47:24 +0530

    frontend 01

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import ProductCard from './components/productCard'
+
+function App() {
+
+
+  return (
+    <>
+      <ProductCard name="Apple iPad" price="$499" image="https://imagedelivery.net/7D3NQGy_afPWwbfcO5Acjw/celltronics.lk/2024/05/iPad-Air-M2-Chip-1.jpg/w=800,h=800,fit=crop"/>
+    
+      <ProductCard name="Mac Book Pro" price="$1299" image="https://imagedelivery.net/7D3NQGy_afPWwbfcO5Acjw/celltronics.lk/2022/06/07-3.jpg/w=800,h=800,fit=crop"/>
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/productCard.jsx
@@ -0,0 +1,16 @@
+import "./productCard.css"
+export default function ProductCard(props){
+
+    console.log(props.price)
+
+    return(
+        <div className="productCard">
+            <h1>{props.name}</h1>
+            <p>{props.price}</p>
+            <img
+                className="productImage"
+                src="https://imagedelivery.net/7D3NQGy_afPWwbfcO5Acjw/celltronics.lk/2024/05/iPad-Air-M2-Chip-1.jpg/w=800,h=800,fit=crop"/>
+            <button>Add to cart</button>
+        </div>
+    )
+}
