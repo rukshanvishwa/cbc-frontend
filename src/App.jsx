93c86cf98ec5a004d40286c7f6939924f7339505
rukshanvishwa <rukshanprakash123@gmail.com>
2025-10-06 01:22:46 +0530

    Add react-hot-toast for notifications and enhance login page UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,23 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import ProductCard from "./components/productCard";
 import AdminPage from "./pages/adminPage";
+// import HomePage from "./pages/homePage";
 import TestPage from "./pages/test";
-import HomePage from "./pages/homepage";
 import LoginPage from "./pages/loginPage";
+import { Toaster } from "react-hot-toast";
+import HomePage from "./pages/homepage";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<div className="w-full h-[100vh] ">
-
+				<Toaster position="top-right"/>
 				<Routes path="/">
 					<Route path="/*" element={<HomePage/>}/>
 					<Route path="/register" element={<h1>Register page</h1>} />
-          <Route path="/login" element={<LoginPage/>} />
+					<Route path="/login" element={<LoginPage/>} />
 					<Route path="/admin/*" element={<AdminPage/>} />
-					<Route path="/test" element={<TestPage/>} />			
+					<Route path="/test" element={<TestPage/>}/>			
 				</Routes>
 
 			</div>
